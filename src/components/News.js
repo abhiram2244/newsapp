79e7dc746b4a412991447e43298083f7a56952a6
Abhiram <koraabhiram@gmail.com>
2023-07-05 12:32:36 +0530

    added previous and next buttons

--- src/components/News.js
+++ src/components/News.js
@@ -2,82 +2,72 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [{
-    "source": {
-      "id": "bbc-sport",
-      "name": "BBC Sport"
-    },
-    "author": null,
-    "title": "'Edgbaston provides joy in cricket's difficult week'",
-    "description": "Why there were so many positives on show at Edgbaston following a difficult week for cricket.",
-    "url": "http://www.bbc.co.uk/sport/cricket/66077048",
-    "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/15DC0/production/_130263598_wyatt.jpg",
-    "publishedAt": "2023-07-01T22:22:16.6836749Z",
-    "content": "Women's Ashes 2023: ICEC report needs to be a catalyst for change\r\nFor England, it may not have been the desired result, but it certainly had the desired effect.\r\nThere was an all-female groundstaff.… [+3902 chars]"
-  },
-  {
-    "source": {
-      "id": "news24",
-      "name": "News24"
-    },
-    "author": "sport",
-    "title": "Dramatic fall from grace: Shock as West Indies miss out on World Cup after Scotland loss",
-    "description": "The West Indies failed to qualify for the Cricket World Cup for the first time as the two-time champions slumped to a seven-wicket thumping by Scotland at the qualifying event on Saturday.",
-    "url": "https://www.news24.com/sport/cricket/dramatic-fall-from-grace-shock-as-west-indies-miss-out-on-world-cup-after-scotland-loss-20230701",
-    "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/2912/b05436dedc1b47dc8f2b0d088e3d552d.jpg",
-    "publishedAt": "2023-07-01T16:13:47+00:00",
-    "content": "<ul><li>The West Indies made unwanted history on Saturday after they failed to qualify for this year's 50-over World Cup for the first time.</li><li>The outcome will come as a massive shock to the Ca… [+3023 chars]"
-  },
-  {
-    "source": {
-      "id": "espn-cric-info",
-      "name": "ESPN Cric Info"
-    },
-    "author": null,
-    "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-    "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-    "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-    "publishedAt": "2020-04-27T11:41:47Z",
-    "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-  },
-  {
-    "source": {
-      "id": "espn-cric-info",
-      "name": "ESPN Cric Info"
-    },
-    "author": null,
-    "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-    "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-    "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-    "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-    "publishedAt": "2020-03-30T15:26:05Z",
-    "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-  }]
+
 
   constructor() {
     super();
     console.log("Hello I am a constructor from news component.");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
+      pageSize: 20,
+
 
     }
   }
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=8c127ee32275476d827e7da6e1bd0222&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({ articles: parseData.articles, totalResults: parseData.totalResults })
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8c127ee32275476d827e7da6e1bd0222&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles,
+
+    })
+
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pageSize)) {
+
+    }
+    else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8c127ee32275476d827e7da6e1bd0222&page=${this.state.page + 1}&pageSize=${this.state.pageSize}`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles,
+
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container" >
-        <h2>NewMonkey - Top Headlines</h2>
+        <h1>NewMonkey - Top Headlines</h1>
 
         <div className="row">
           {this.state.articles.map((element) => {
 
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage}
+              <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage}
                 newsUrl={element.url} />
             </div>
 
           })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }} >
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl ? "https://cdn.finshots.app/images/2023/07/design-103-gift.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body" >
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read more</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read more</a>
           </div>
         </div>
       </div>
